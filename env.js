babae0ac1873c5e2f4304e7791f9e97b0355ca6b
Phung Van Quy <phungvanquy97@gmail.com>
2024-10-13 04:25:07 +0300

    first commit

---
+++ env.js
@@ -0,0 +1,17 @@
+window.settings = {
+  // 站点标题
+  title: 'V2Board',
+  // API
+  host: 'https://v2board.phungvanquy.com',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  // 背景
+  background_url: '',
+  logo: '',
+  // 需与V2Board设置中的后台路径一致
+  secure_path: 'admincontrol'
+}
